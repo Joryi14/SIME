063cc55e0c1e4c21834f4f07dcaa706fc14abb74
joryi14 <joryimed19@gmail.com>
2019-08-22 20:07:35 -0600

    Algunos Modelos

---
+++ app/Models/permiso.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class permiso extends Model
+{
+    //
+    protected $table = 'permiso';
+}
---
+++ app/Models/rol.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class rol extends Model
+{
+    //
+    protected $table = 'rol';
+}

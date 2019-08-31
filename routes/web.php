31bf89ab8229ea4fa6268746981301f35baddecf
joryi14 <joryimed19@gmail.com>
2019-08-31 11:30:15 -0600

    Nuevo Login

--- routes/web.php
+++ routes/web.php
@@ -21,26 +21,9 @@ Route::group(['prefix' => '/'], function () {
 });
 Route::group(['prefix' => 'admin','namespace' => 'Admin'], function () {
     /*Permiso*/ 
-    Route::get('permiso', 'permisoController@index');
-    Route::post('permiso/store','permisoController@store');
-    Route::put('/{permiso}','permisoController@update');
-    /* Menu*/ 
-    Route::get('Menu', 'MenuController@index');
-    Route::get('Menu/Create','MenuController@Create')->name('menu_create');
-    Route::post('Menu','MenuController@Store')->name('menu_guardar');
-    Route::get('Menu/{id}/editar', 'MenuController@edit')->name('menu_edit');
-    Route::put('Menu/{id}', 'MenuController@update')->name('menu_update');
-    Route::get('Menu/{id}/destroy', 'MenuController@destroy')->name('menu_delete');
-    Route::post('Menu/guardar-orden', 'MenuController@guardarOrden')->name('guardar_orden');
-    /*Rol*/ 
-    Route::get('rol', 'rolController@index')->name('rol_index');
-    Route::get('rol/Create','rolController@Create')->name('rol_create');
-    Route::post('rol','rolController@Store')->name('rol_guardar');
-    Route::get('rol/{id}/edit', 'rolController@edit')->name('rol_edit');
-    Route::put('rol/{id}', 'rolController@update')->name('rol_update');
-    Route::delete('rol/{id}', 'rolController@delete')->name('rol_delete');
-    /*MenuRol*/
-    Route::get('menurol', 'MenurolController@index')->name('menurol_index');
-    Route::post('menurol','rolController@Store')->name('menurol_guardar');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

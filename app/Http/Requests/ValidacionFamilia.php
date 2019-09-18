6a545ad726e2cfda190d80c49d3125d7147f4562
moni3312 <moniesvel@gmail.com>
2019-09-18 11:25:25 -0600

    cedula

--- app/Http/Requests/ValidacionFamilia.php
+++ app/Http/Requests/ValidacionFamilia.php
@@ -32,7 +32,7 @@ class ValidacionFamilia extends FormRequest
             'Parentesco'=> 'required',
             'Edad'=> 'required|max:3',
             'sexo'=> 'required',
-           
+            'Patologia'=> 'required|max:15'
         ];
     }
     public function messages()
@@ -61,7 +61,8 @@ class ValidacionFamilia extends FormRequest
             
             'sexo.required' => 'El sexo es requerido',
          
-           
+            'Patologia.required' => 'La Patologia es requerida',
+            'Patologia.max' => 'La Patologia no debe superar los 15 digitos'
 
         ];
 

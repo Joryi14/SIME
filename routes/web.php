42a141fe60fb8f17d7f544cc0c72e43ae37db1e8
moni3312 <moniesvel@gmail.com>
2019-09-30 03:52:44 -0600

    reporte de inventario

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::group(['prefix' => '/','middleware' => ['role:Admin|Director|Lider Comuna
     Route::get('Retiro_PaquetesV/{Retiro_PaquetesV}/edit', 'Retiro_PaquetesVController@edit');
     Route::put('Retiro_PaquetesV/{Retiro_PaquetesV}','Retiro_PaquetesVController@update');
     Route::delete('Retiro_PaquetesV/{Retiro_PaquetesV}','Retiro_PaquetesVController@delete')->name('Retiro_PaquetesV_delete');
-    Route::get('pdf', 'Retiro_PaquetesVController@generar')->name('Retiro_PaquetesV_reporte');
+    Route::get('Retiro_PaquetesV/pdf', 'Retiro_PaquetesVController@generar')->name('Retiro_PaquetesV_reporte');
 });
 Route::group(['prefix' => '/'], function () {
     Route::get('Inventario', 'InventarioController@index')->name('inicio_inventario');
@@ -42,6 +42,7 @@ Route::group(['prefix' => '/'], function () {
     Route::get('Inventario/{Inventario}/edit', 'InventarioController@edit');
     Route::put('Inventario/{Inventario}','InventarioController@update');
     Route::delete('Inventario/{Inventario}','InventarioController@delete')->name('inventario_delete');
+    Route::get('Inventario/pdf', 'InventarioController@generar')->name('inventario_reporte');
     
 });
 Route::group(['prefix' => '/'], function () {

71306e45e5a7bf7ae729154d08411d9755d69bc7
moni3312 <moniesvel@gmail.com>
2019-10-01 02:15:45 -0600

    e

--- resources/views/Inventario/create.blade.php
+++ resources/views/Inventario/create.blade.php
@@ -1,22 +1,23 @@
 @extends("theme/$theme/layout")
 @section('Contenido')
 <form class= "form-group" method="POST" action="/Inventario/store">
+  @method('PUT')
   @csrf
 <form>
 <div class= "form-group">
   <h1>Crear reporte de incidente</h1>
   <div class="form-group ">
-  <label>idEmergencias: <input type="text" name="idEmergencias" class= "form-control" > </label><br>
+  <label>Id de la emergencias: <input type="text" name="idEmergencias" class= "form-control"  value="{{json_decode($ideme)}}"> </label><br>
   <label>Suministros: <input type="text" name="Suministros" class= "form-control" > </label><br>
 
   <input type="hidden" name="Colchonetas" value="0" />
-  <label><input type="checkbox" name="Colchonetas" value="1"> Colchonetas</label><br>
+  <label><input type="checkbox" name="Colchonetas" value="1"> Colchonetas: </label><br>
 
   <input type="hidden" name="Cobijas" value="0" />
-  <label><input type="checkbox" name="Cobijas" value="1"> Cobijas</label><br>
+  <label><input type="checkbox" name="Cobijas" value="1"> Cobijas: </label><br>
 
   <input type="hidden" name="Ropa" value="0" />
-  <label><input type="checkbox" name="Ropa" value="1"> Ropa</label><br>
+  <label><input type="checkbox" name="Ropa" value="1"> Ropa: </label><br>
    </div>
   
    <button type="submit" class="btn btn-primary">Guardar</button>

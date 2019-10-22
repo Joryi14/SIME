9e18866f586be677cbc818efc0736cb6e6b41425
moni3312 <moniesvel@gmail.com>
2019-10-21 23:08:42 -0600

    a

--- resources/views/Retiro_PaquetesV/reporte.blade.php
+++ resources/views/Retiro_PaquetesV/reporte.blade.php
@@ -22,7 +22,7 @@
                 background: #dedede;
             }
             #header { position: fixed; left: 0px; top: -180px; right: 0px; height: 150px; background-color: orange; text-align: center; }
-            #footer { position: fixed; left: 0px; bottom: -180px; right: 0px; height: 150px; background-color: orange; }
+            #footer { position: fixed; left: 0px; bottom: -180px; right: 0px; height: 80px; background-color: orange; }
              #footer .page:after { content: counter(page, upper-roman); }
 
         </style>
